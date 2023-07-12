79739ffc531bbe7cf1757d3a8ca03b0cd53879c0
georgepiter <george.piter@terra.com.br>
2023-07-12 20:20:20 -0300

    add structure project

---
+++ src/main/java/br/com/fightlab/FightlabApplication.java
@@ -0,0 +1,13 @@
+package br.com.fightlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightlabApplication.class, args);
+	}
+
+}
